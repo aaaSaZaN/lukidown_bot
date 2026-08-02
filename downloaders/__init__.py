a544dd2bb955812378cbde42ca33a13d2984c24b
Luki <me@lukiuwu.xyz>
2026-08-02 19:25:46 +0300

    добавлен __all__ в downloaders/__init__.py

--- downloaders/__init__.py
+++ downloaders/__init__.py
@@ -15,3 +15,22 @@ from downloaders.http import aclose_http_clients
 from downloaders.kinopoisk import fetch_kinopoisk_info, list_episodes, list_seasons
 from downloaders.router import cleanup, download
 
+__all__ = [
+    "AUDIO_FORMATS",
+    "VIDEO_QUALITIES",
+    "CancelCheck",
+    "DownloadCancelled",
+    "DownloadResult",
+    "FileTooLarge",
+    "ProgressCallback",
+    "aclose_http_clients",
+    "cleanup",
+    "download",
+    "fetch_kinopoisk_info",
+    "get_available_audio_codecs",
+    "get_available_video_heights",
+    "list_episodes",
+    "list_seasons",
+]
+
+
